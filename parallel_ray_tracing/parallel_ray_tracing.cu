d54c7e093efad79aa331fae0ff5192720eb8df09
Lalit Rajendran <lrajendr@purdue.edu>
2016-04-21 10:01:20 -0400

    test commit from nsight. added typedefs for structures

---
+++ parallel_ray_tracing/parallel_ray_tracing.cu
@@ -0,0 +1,24 @@
+/*
+ * parallel_ray_tracing.cu
+ *
+ *  Created on: Apr 20, 2016
+ *      Author: lrajendr
+ */
+#include<"parallel_ray_tracing.h">
+
+extern "C"{
+
+lightfield_data_t* start_ray_tracing(float lens_pitch, float image_distance,
+		scattering_data_t* scattering_data, char* scattering_type,
+		lightfield_source_t* lightfield_source, int lightray_number_per_particle,
+		int n_min, int n_max)
+{
+
+
+
+}
+
+
+}
+
+
