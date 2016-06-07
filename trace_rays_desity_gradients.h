f7969e9d6dde4041b77d2ca66b3ae3ca4d46d83f
lrajendr <lrajendr@purdue.edu>
2016-06-07 19:34:07 -0400

    removed duplicate function to check ray tracing through density gradients

---
+++ trace_rays_desity_gradients.h
@@ -0,0 +1,13 @@
+/*
+ * trace_rays_desity_gradients.h
+ *
+ *  Created on: May 23, 2016
+ *      Author: lrajendr
+ */
+
+#ifndef TRACE_RAYS_DESITY_GRADIENTS_H_
+#define TRACE_RAYS_DESITY_GRADIENTS_H_
+
+
+
+#endif /* TRACE_RAYS_DESITY_GRADIENTS_H_ */
