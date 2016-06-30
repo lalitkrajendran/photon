e03d4958939a823d9bca0edf63833de980a70557
lrajendr <lrajendr@purdue.edu>
2016-06-29 20:40:30 -0400

    removed unnecessary includes

--- Debug/subdir.mk
+++ Debug/subdir.mk
@@ -17,8 +17,8 @@ OBJS += \
 %.o: ../%.cu
 	@echo 'Building file: $<'
 	@echo 'Invoking: NVCC Compiler'
-	/opt/cuda/7.0/bin/nvcc -I/home/barracuda/a/lrajendr/usr/include -I/usr/include -I/usr/lib64 -I/usr/local/lib -I../ -I/home/barracuda/a/lrajendr/usr/lib -I/home/barracuda/a/lrajendr/usr/ -G -g -O0 -maxrregcount 32 -Xcompiler -fPIC -gencode arch=compute_20,code=sm_20  -odir "." -M -o "$(@:%.o=%.d)" "$<"
-	/opt/cuda/7.0/bin/nvcc -I/home/barracuda/a/lrajendr/usr/include -I/usr/include -I/usr/lib64 -I/usr/local/lib -I../ -I/home/barracuda/a/lrajendr/usr/lib -I/home/barracuda/a/lrajendr/usr/ -G -g -O0 -maxrregcount 32 -Xcompiler -fPIC --compile --relocatable-device-code=false -gencode arch=compute_20,code=compute_20 -gencode arch=compute_20,code=sm_20  -x cu -o  "$@" "$<"
+	/opt/cuda/7.0/bin/nvcc -I/usr/include -I/usr/lib64 -I/usr/local/lib -I../ -G -g -O0 -maxrregcount 32 -Xcompiler -fPIC -gencode arch=compute_20,code=sm_20  -odir "." -M -o "$(@:%.o=%.d)" "$<"
+	/opt/cuda/7.0/bin/nvcc -I/usr/include -I/usr/lib64 -I/usr/local/lib -I../ -G -g -O0 -maxrregcount 32 -Xcompiler -fPIC --compile --relocatable-device-code=false -gencode arch=compute_20,code=compute_20 -gencode arch=compute_20,code=sm_20  -x cu -o  "$@" "$<"
 	@echo 'Finished building: $<'
 	@echo ' '
 
