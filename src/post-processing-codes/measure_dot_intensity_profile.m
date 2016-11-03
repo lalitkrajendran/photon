093c9a85d0766038a8ebe4bd792df59712be05ad
Lalit Rajendran <lrajendr@purdue.edu>
2016-11-03 18:34:36 -0400

    removed lines that performed an initial sizing using the 3pt gaussian method, because this is done by default in Leastsqrmethods.m

--- src/post-processing-codes/measure_dot_intensity_profile.m
+++ src/post-processing-codes/measure_dot_intensity_profile.m
@@ -29,19 +29,19 @@ dot_diameters_physical = [10 20 30 40 50 100 150 200 250 300 350 400 450 500 550
 %% set read and write paths
 
 % this is the path to the folder containing the raw images
-img_filepath = ['~/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/cropped-images/'];
+img_filepath = ['/home/barracuda/a/lrajendr/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/cropped-images/'];
 
 % this is the folder where the particle identification results will be stored
-particle_id_filepath = ['~/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/results/particle-id/'];
+particle_id_filepath = ['/home/barracuda/a/lrajendr/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/results/particle-id/'];
 
 % this is the folder where the particle sizing results will be stored
-particle_size_filepath = ['~/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/results/particle-size/'];
+particle_size_filepath = ['/home/barracuda/a/lrajendr/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/results/particle-size/'];
 
 % this is the folder where the figures will be saved
-figure_save_filepath = ['~/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/plots/'];
+figure_save_filepath = ['/home/barracuda/a/lrajendr/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/plots/'];
 
 % this is the folder where the workspace variables will be saved
-workspace_filepath = ['~/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/results/'];
+workspace_filepath = ['/home/barracuda/a/lrajendr/Projects/camera_simulation/results/images/bos/error-analysis/dot-size/processing/' case_name '/results/'];
 
 % this creates the write directories if they are not already present
 if ~exist(particle_id_filepath,'dir')
@@ -105,6 +105,7 @@ for i = 1:1+skip:N
     % this updates the number of images read
     image_ctr = image_ctr + 1;
     waitbar(image_ctr/num_analysis);
+    
     %% locate dots and measure diameter
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
@@ -125,31 +126,15 @@ for i = 1:1+skip:N
     Data.ID.save_dir       = particle_id_filepath;
 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-    % size the particles [intial guess]
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
-
-    % define settings for particle sizing
-    Data.Size.run      = 1;
-    Data.Size.thresh   = 10;
-    Data.Size.method   = 'IWC';
-    Data.Size.p_area   = 0;
-    Data.Size.sigma    = 4;
-    Data.Size.errors   = 1;%str2double(Data.Size.errors);
-
-    sizeprops       = Data.Size;
-    % call function to size the particles
-    [SIZE1.XYDiameter,SIZE1.mapsizeinfo,SIZE1.locxy]=particle_sizing(img,ID1.p_matrix,...
-                        ID1.num_p,sizeprops);
-
-    %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
     % size the particles 
     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
     % define settings for particle sizing
+    SIZEmethod={'GEO','IWC','TPG','FPG','CFPG','LSG','CLSG'};
     Data.Size.run      = 1;
     Data.Size.thresh   = 10;
     Data.Size.method   = 'CLSG';
-    Data.Size.p_area   = mean(SIZE1.XYDiameter(:,3));
+    Data.Size.p_area   = 0;
     Data.Size.sigma    = 4;
     Data.Size.errors   = 1;%str2double(Data.Size.errors);
 
@@ -343,11 +328,6 @@ for i = 1:1+skip:N
 %     surf(I_dot_average, 'linestyle','none');
 %     view(2)
 %     title('full')
-% 
-% % find average and std of intensity profile
-% 
-% 
-% 
 
 end
 %%
