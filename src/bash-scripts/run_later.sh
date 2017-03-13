e8e7e0c03307683c4beb1f0f3845b50f7c1c4dc2
Lalit Rajendran <lrajendr@purdue.edu>
2017-03-13 14:01:25 -0400

    moved codes to single folder

--- src/bash-scripts/run_later.sh
+++
@@ -1,5 +0,0 @@
-# generate bos parameters (make sure to specify the correct grad_x)
-python batch_create_bos_params.py 
-
-# generate images
-./batch_generate_images.sh
--- src/batch.sh
+++
@@ -1,5 +0,0 @@
-python createNRRD.py
-python run_piv_challenge_simulation_01.py
-cp img*1.png bos_test.png
-python compareImages.py
-eog bos_comparison.png
