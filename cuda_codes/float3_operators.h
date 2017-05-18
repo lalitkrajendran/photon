efca73195b2c33f8549aa9988b18458f55b50641
lrajendr <lrajendr@purdue.edu>
2017-05-18 12:20:11 -0400

    commented out functions in float3_operators.h which were clashing with the ones in cutil_mathbug_fixes.h that is a part of the CI package.

--- cuda_codes/float3_operators.h
+++ cuda_codes/float3_operators.h
@@ -13,49 +13,48 @@
 
 #include <string>
 
-__device__ float3 operator/(const float3 &a, const float &b) {
-
-return make_float3(a.x/b, a.y/b, a.z/b);
-
-}
-
-__device__ float3 operator*(const float3 &a, const float &b) {
-
-return make_float3(a.x*b, a.y*b, a.z*b);
-
-}
-
-__device__ float3 operator*(const float &b,const float3 &a) {
-
-return make_float3(a.x*b, a.y*b, a.z*b);
-
-}
-
-__device__ float3 operator+(const float3 &a, const float3 &b) {
-
-return make_float3(a.x+b.x, a.y+b.y, a.z+b.z);
-
-}
-
-__device__ float3 operator-(const float3 &a, const float3 &b) {
-
-return make_float3(a.x-b.x, a.y-b.y, a.z-b.z);
-
-}
-
-
+//__device__ float3 operator/(const float3 &a, const float &b) {
+//
+//return make_float3(a.x/b, a.y/b, a.z/b);
+//
+//}
+//
+//__device__ float3 operator*(const float3 &a, const float &b) {
+//
+//return make_float3(a.x*b, a.y*b, a.z*b);
+//
+//}
+//
+//__device__ float3 operator*(const float &b,const float3 &a) {
+//
+//return make_float3(a.x*b, a.y*b, a.z*b);
+//
+//}
+//
+//__device__ float3 operator+(const float3 &a, const float3 &b) {
+//
+//return make_float3(a.x+b.x, a.y+b.y, a.z+b.z);
+//
+//}
+//
+//__device__ float3 operator-(const float3 &a, const float3 &b) {
+//
+//return make_float3(a.x-b.x, a.y-b.y, a.z-b.z);
+//
+//}
+//
 __device__ float3 operator-(const float3 &a) {
 
 return make_float3(-a.x, -a.y, -a.z);
 
 }
 
-__device__ bool operator==(const float3&a, const float3&b) {
-	if(a.x==b.x && a.y==b.y && a.z==b.z)
-		return true;
-
-	return false;
-}
+//__device__ bool operator==(const float3&a, const float3&b) {
+//	if(a.x==b.x && a.y==b.y && a.z==b.z)
+//		return true;
+//
+//	return false;
+//}
 
 __device__ bool operator==(const float4&a, const float4&b) {
 	if(a.x==b.x && a.y==b.y && a.z==b.z && a.w==b.w)
@@ -64,20 +63,18 @@ __device__ bool operator==(const float4&a, const float4&b) {
 	return false;
 }
 
-
-__device__ float3 normalize(const float3&a) {
-    float m = sqrt(a.x*a.x+a.y*a.y+a.z*a.z);
-    return a/m;
-}
-
-__device__ float3 cross(const float3&a, const float3&b) {
-    return make_float3(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x);
-}
-
-
-__device__ float dot(const float3&a, const float3&b) {
-    return (a.x*b.x + a.y*b.y + a.z*b.z);
-}
+//__device__ float3 normalize(const float3&a) {
+//    float m = sqrt(a.x*a.x+a.y*a.y+a.z*a.z);
+//    return a/m;
+//}
+//
+//__device__ float3 cross(const float3&a, const float3&b) {
+//    return make_float3(a.y*b.z-a.z*b.y, a.z*b.x-a.x*b.z, a.x*b.y-a.y*b.x);
+//}
+//
+//__device__ float dot(const float3&a, const float3&b) {
+//    return (a.x*b.x + a.y*b.y + a.z*b.z);
+//}
 
 __device__ float3 fabs(const float3&a)
 {
@@ -87,15 +84,16 @@ __device__ float3 fabs(const float3&a)
 __device__ float angleBetween(const float3& v1, const float3& v2)
 {
   // returns angle between two vectors in degrees
-  float dp = dot(v1, v2);
+//  float dp = dot(v1, v2);
+	float dp = v1.x*v2.x + v1.y*v2.y + v1.z*v2.z;
   return acosf(dp)*180.0/M_PI;
 }
 
-void print3(char* msg,float3 v)
-{
-//#if __DEVICE_EMULATION__
-  printf("%s",msg);
-  printf(": %f %f %f\n", v.x, v.y, v.z);
-//#endif
-}
+//void print3(char* msg,float3 v)
+//{
+////#if __DEVICE_EMULATION__
+//  printf("%s",msg);
+//  printf(": %f %f %f\n", v.x, v.y, v.z);
+////#endif
+//}
 #endif /* FLOAT3_OPERATORS_H_ */
